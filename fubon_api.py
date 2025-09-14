fcb1cdc9a215bef121553e785a66ebdff6617a00
jenting0903 <jenting0903@outlook.com>
2025-09-14 20:48:58 +0800

    修改查詢帳務指令

--- fubon_api.py
+++ fubon_api.py
@@ -19,30 +19,30 @@ class FubonAdventure:
             print("❌ 登入失敗：", e)
             self.account = None
 
-def query_account(self):
-    if not self.account:
-        print("⚠️ 帳戶為空，無法查詢")
-        return None
-    try:
-        print("📦 呼叫 get_bank_remain() 中...")
-        result = self.sdk.account.get_bank_remain(self.account)
-        print("📦 銀行帳戶回傳：", result)
-
-        if not result.is_success or not result.data:
-            print("❌ 查詢失敗：", result.message)
+    def query_account(self):
+        if not self.account:
+            print("⚠️ 帳戶為空，無法查詢")
             return None
+        try:
+            print("📦 呼叫 get_bank_remain() 中...")
+            result = self.sdk.account.get_bank_remain(self.account)
+            print("📦 銀行帳戶回傳：", result)
 
-        bank = result.data
-        return {
-            "branch": bank.branch_no,
-            "account": bank.account,
-            "currency": bank.currency,
-            "balance": int(bank.balance),
-            "available": int(bank.available_balance)
-        }
-    except Exception as e:
-        print("❌ 查詢帳務失敗：", e)
-        return None
+            if not result.is_success or not result.data:
+                print("❌ 查詢失敗：", result.message)
+                return None
+
+            bank = result.data
+            return {
+                "branch": bank.branch_no,
+                "account": bank.account,
+                "currency": bank.currency,
+                "balance": int(bank.balance),
+                "available": int(bank.available_balance)
+            }
+        except Exception as e:
+            print("❌ 查詢帳務失敗：", e)
+            return None
 
 
 
